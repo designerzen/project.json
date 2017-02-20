860b7aa78a18062a90f72e2eb2d00f5efc279efd
designerzen <designerzen@gmail.com>
2017-02-20 17:58:46 +0000

    Stubs
    Barebones files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # project.json
+
 A data model for public consumption that highlights the best things about this project
+
+
+
+---
+
+Here is a breakdown of what is in the [project.json]() file
---
+++ test.js
@@ -0,0 +1,7 @@
+// This is a simple script that loads up an interface model of the
+// required data types and makes sure that there are :
+// - All neccessary keys
+// - Correct data types in the fields
+// - Valid syntax
+
+// So to begin, we will grab all of the files ending with json in the tests folder
